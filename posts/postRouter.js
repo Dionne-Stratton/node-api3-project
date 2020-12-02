509578a958877fa1ae2077ffcf7875d7c6f41d6f
Dionne Stratton <mylife33@hotmail.com>
2020-12-02 17:52:39 -0600

    mostly done

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,77 @@
-const express = require('express');
-
+const express = require("express");
+const Posts = require("./postDb");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get("/", (req, res) => {
+  const { query } = req;
+  Posts.get(query)
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err.message });
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get("/:id", validatePostId, (req, res) => {
+  const { id } = req.params;
+
+  Posts.getById(id)
+    .then((post) => {
+      res.status(200).json(post);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err.message });
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete("/:id", validatePostId, (req, res) => {
+  const { id } = req.params;
+
+  Posts.remove(id)
+    .then((nuke) => {
+      res.status(200).json({ message: "This post is gone forever!" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err.message });
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put("/:id", validatePostId, (req, res) => {
+  const { id } = req.params;
+  const edit = req.body;
+
+  Posts.update(id, edit)
+    .then((post) => {
+      res.status(200).json(post);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err.message });
+    });
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  const { id } = req.params;
+
+  Posts.getById(id)
+    .then((postId) => {
+      if (postId) {
+        req.id == id;
+        next();
+      } else {
+        res.status(404).json({ message: `post with id ${id} not found` });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err.message });
+    });
 }
 
 module.exports = router;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,71 @@
-const express = require('express');
-
+const express = require("express");
+const Users = require("./userDb");
+const Posts = require("../posts/postDb");
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  const { id } = req.params;
+
+  Users.getById(id)
+    .then((userId) => {
+      if (userId) {
+        req.id == id;
+        next();
+      } else {
+        res.status(404).json({ message: "user not found" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if (!req.body.name) {
+    res.status(400).json({ message: "name required" });
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if (!req.body) {
+    res.status(400).json({ message: "form fields required" });
+  } else if (!req.body.text) {
+    res.status(400).json({ message: "text field required" });
+  } else {
+    next();
+  }
 }
 
 module.exports = router;
